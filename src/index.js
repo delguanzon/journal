490124d4d9708f05cfadfa6af57252cfc4019684
delguanzon <yodelguanzon@gmail.com>
2022-11-07 14:04:55 -0800

    Add methods for getting vowel and consonant count
    Co-authored-by: Henry Sullivan <sullivanbhenry@gmail.com>

---
+++ src/index.js
@@ -0,0 +1,18 @@
+import Entry from './journal.js';
+
+//document.getElementById("word-count").replaceChildren("test");
+
+function handleInput(event) {
+  event.preventDefault();
+  let title = document.getElementById("title-input").value;
+  let passage = document.getElementById("passage-input").value;
+  let entry = new Entry(passage,title);
+  let pElement = document.getElementById("word-count");
+  pElement.replaceChildren(entry.getWordCount(passage));
+
+  let vowelElement = document.getElementById("vowel-count");
+  vowelElement.replaceChildren(entry.getVowelConsCount(passage));
+}
+
+let form = document.getElementById("input-form");
+form.addEventListener("submit", handleInput);
---
+++ src/journal.js
@@ -0,0 +1,39 @@
+//1. Create Entry objects that include a method to return the number of words in the entry.
+//2. Add a separate method (or methods) to return the number of vowels and consonants in each entry
+//3. Finally, add a method called getTeaser to return the first sentence of the entry.
+//   3.a If the sentence is over 8 words, only display those first 8 words.
+
+export default function Entry(body, title) {
+  this.body = body;
+  this.title = title;
+}
+
+Entry.prototype.getWordCount = function () {
+  return this.body.split(" ").length;
+};
+
+Entry.prototype.getVowelConsCount = function () {
+  // let regex = /[a,e,i,o,u]\w/;
+  // this.body.match(regex);
+
+  let vowels = ['a','e','i','o','u'];
+  let ctrVowel = 0;
+
+  let ctrCons = 0;
+  let text = this.body.split(" ").join("").split("");
+  text.forEach(function (letter) {
+    let isNotVowel = true;
+    vowels.forEach(function (element) {      
+      if(letter.toLowerCase() === element) {
+        ctrVowel++;
+        isNotVowel = false;
+      }
+    });
+
+    if(isNotVowel) {
+      ctrCons++;
+      
+    }
+  });
+  return "Vowels: " + ctrVowel + ", Consonants: " + ctrCons;
+};
